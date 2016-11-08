c6929ff347c235f79c59b3fe2045ae1ee4e5ccf8
duswjd017427 <duswjd017427@gmail.com>
2016-11-08 15:57:10 +0900

    다삭제

---
+++ BasaServceMain/src/com/badaadmin/controller/member/MemberList.java
@@ -0,0 +1,19 @@
+package com.badaadmin.controller.member;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.badaservice.helper.BaseController;
+
+public class MemberList extends BaseController {
+
+	@Override
+	public String doRun(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
